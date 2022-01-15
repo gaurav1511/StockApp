626b274e2c89ed2f6deca28e3de1a4cc9c047b86
Gaurav <1511.gaurav@gmail.com>
2022-01-15 15:47:51 +0530

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Gaurav Singh on 15/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
